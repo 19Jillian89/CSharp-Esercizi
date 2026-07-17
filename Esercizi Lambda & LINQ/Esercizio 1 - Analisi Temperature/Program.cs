cedf4dffc453e134bb31924812532052f8cc6cb0
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-07-17 09:25:10 +0200

    Add Citta class for temperature analysis

---
+++ Esercizi Lambda & LINQ/Esercizio 1 - Analisi Temperature/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analisi_Temperature
+{
+    public class Citta
+    {
+        public string Nome { get; set; }
+        public double Temperature { get; set; }
+        public double Umidita { get; set; }
+    }
+}
