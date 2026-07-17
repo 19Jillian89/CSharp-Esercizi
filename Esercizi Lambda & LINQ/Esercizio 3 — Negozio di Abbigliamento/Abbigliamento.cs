91fd1fec52e3666d8758db593e42471b73d4017e
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-07-17 09:40:48 +0200

    Create Abbigliamento class with properties

---
+++ Esercizi Lambda & LINQ/Esercizio 3 — Negozio di Abbigliamento/Abbigliamento.cs
@@ -0,0 +1,8 @@
+public class Abbigliamento
+{
+    public string Modello { get; set; }
+    public string Reparto { get; set; }
+    public int Disponibilita { get; set; }
+    public decimal PrezzoOriginale { get; set; }
+
+}
